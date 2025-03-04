d511fbd83d42a5c2ffbda59a70e0f4244a732459
tollboxlap09 <eslammohamdeen89@gmail.com>
2025-03-04 02:10:12 +0200

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,38 @@
+@echo off
+
+:START_ANYDESK
+anydesk --start
+powershell -Command "Start-Sleep -Seconds 5"
+for /f "delims=" %%i in ('"C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe" --get-id') do (
+    set ID=%%i
+)
+if "%ID%"=="" (
+    taskkill /f /im anydesk.exe > nul
+    goto START_ANYDESK
+)
+if "%ID%"=="0" (
+    taskkill /f /im anydesk.exe > nul
+    goto START_ANYDESK
+)
+
+echo disalardp | anydesk.exe --set-password _full_access
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(147, 489, duration=2)"
+python -c "import pyautogui as pag; pag.click(156, 552, duration=2)"
+python -c "import pyautogui as pag; pag.click(587, 14, duration=2)"
+python -c "import pyautogui as pag; pag.click(916, 17, duration=2)"
+python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
+
+echo Script By (TOOLBOXLAP) :
+
+echo " Follow us on social media "
+
+echo Blog : https://www.toolboxlap.xyz
+
+echo Youtube : https://www.youtube.com/@TOOLBOXLAP-u1c
+
+echo Facebook : echo FaceBook : https://www.facebook.com/profile.php?id=61567896692994
+echo ..................................................
+echo AnyDesk ID is: %ID%
+echo AnyDesk Password: toolboxlap
+echo You can login now!
